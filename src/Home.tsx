a135923aefc6b8a141bfc75ff9cbbecad96b522f
nicodemos234 <nicodemos54324@gmail.com>
2022-01-23 13:37:02 -0300

    Add async storage and delete option

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react"
-import { NativeSyntheticEvent, SafeAreaView, Text, TextInputChangeEventData, View } from "react-native"
+import { SafeAreaView, Text, View } from "react-native"
 import styled from "styled-components/native"
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const MainTitle = styled.Text`
   font-size: 40px;
@@ -22,6 +23,18 @@ const SubmitButton = styled.TouchableOpacity`
   margin: 0 20px;
 `
 
+const ListWrapper = styled.View`
+  padding: 20px;
+`
+
+const ItemWrapper = styled.View`
+  margin: 5px 0;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+const ExcludeButton = styled.TouchableOpacity``
+
 const Home = () => {
   const [newTask, setNewTask] = React.useState('')
   const [listOfTasks, setListOfTasks] = React.useState<string[]>([])
@@ -29,18 +42,46 @@ const Home = () => {
   const handleSubmit = React.useCallback(() => {
     setListOfTasks(oldList => [...oldList, newTask])
     setNewTask('')
-  }, [newTask])
-  console.log(listOfTasks)
+  }, [newTask, setListOfTasks, setNewTask])
+
+  const handleExclude = React.useCallback((index: number) => {
+    console.log()
+    setListOfTasks(oldArray => oldArray.filter((_, pos) => pos !== index))
+  }, [setListOfTasks])
+
+  React.useEffect(() => {
+    AsyncStorage.getItem('@todo').then((data) => {
+      if (data) {
+        const parsed = JSON.parse(data)
+        setListOfTasks(parsed)
+      }
+    })
+  }, [])
+
+  React.useEffect(() => {
+    storeData(listOfTasks)
+  }, [listOfTasks])
+
+  const storeData = React.useCallback(async (listOfTasks: string[]) => {
+    try {
+      const jsonValue = JSON.stringify(listOfTasks)
+      await AsyncStorage.setItem('@todo', jsonValue)
+    } catch (e) {
+    }
+  }, [])
 
   return <SafeAreaView>
     <MainTitle>ToDo</MainTitle>
     <Row>
-      <MainInput autoFocus value={newTask} onChangeText={setNewTask} />
+      <MainInput autoFocus value={newTask} onChangeText={setNewTask} onSubmitEditing={handleSubmit} />
       <SubmitButton onPress={handleSubmit}><Text>Add</Text></SubmitButton>
     </Row>
-    {listOfTasks.map((task, index) => <View>
-      <Text key={index}>{task}</Text>
-    </View>)}
+    <ListWrapper>
+      {listOfTasks.map((task, index) => <ItemWrapper key={index}>
+        <Text>{task}</Text>
+        <ExcludeButton onPress={() => handleExclude(index)}><Text>X</Text></ExcludeButton>
+      </ItemWrapper>)}
+    </ListWrapper>
   </SafeAreaView>
 }
 
